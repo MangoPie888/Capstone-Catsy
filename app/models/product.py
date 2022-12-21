d169fc6cec98ca7389cb9ef376a44f4ca6909c76
vivian wang <viviannewang_dy@yahoo.com>
2022-12-20 22:05:35 -0800

    change string length

--- app/models/product.py
+++ app/models/product.py
@@ -10,10 +10,10 @@ class Product(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable = False)
+    name = db.Column(db.String(250), nullable = False)
     price = db.Column(db.Integer, nullable = False)
-    description = db.Column(db.String(255), nullable = False)
-    img = db.Column(db.String(255), nullable= False)
+    description = db.Column(db.String(250), nullable = False)
+    img = db.Column(db.String(250), nullable= False)
 
     seller_id =db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id"), ondelete="CASCADE"))
     shop_id =db.Column(db.Integer,db.ForeignKey(add_prefix_for_prod("shops.id"),ondelete="CASCADE") )
--- app/models/shop.py
+++ app/models/shop.py
@@ -10,7 +10,7 @@ class Shop(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     shop_name = db.Column(db.String(100), nullable=False)
-    shop_description = db.Column(db.String(255),nullable=False)
+    shop_description = db.Column(db.String(250),nullable=False)
     shop_img = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, default = db.func.now())
 
